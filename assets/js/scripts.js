0eab3c0d8bf4d904fc89c9b3bf23b2c046d0543d
Pedro Henrique <38799159+pedrophs2@users.noreply.github.com>
2022-10-14 23:57:38 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,50 @@
+	
+
+	/*------------------- Preloader Start -------------------*/
+
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
+
+	/**-------------------- Back Top ----------------------**/
+
+    $(window).scroll(function(){
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+
+	/*------------------- Navbar Menu -------------------*/
+
+	const hamburger   = document.querySelector('.header .nav-bar .nav-list .hamburger');
+	const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+	const menu_item   = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+	const header      = document.querySelector('.header.hero');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+
+	document.addEventListener('scroll', () => {
+		var scroll_position = window.scrollY;
+		if (scroll_position > 250) {
+			header.style.backgroundColor = '#000000';
+		} else {
+			header.style.backgroundColor = 'transparent';
+		}
+	});
+
+	menu_item.forEach((item) => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('active');
+			mobile_menu.classList.toggle('active');
+		});
+	});
